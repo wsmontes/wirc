b07ca238ea6d30008b1660bcc5c890e69dfd8d3c
Wagner Montes <wmontes@gmail.com>
2026-06-21 18:02:56 -0700

    fix: log invalid hex colors, improve pencil contrast to pass WCAG AA

--- Wirc/WircApp/Core/DesignSystem.swift
+++ Wirc/WircApp/Core/DesignSystem.swift
@@ -1,3 +1,4 @@
+import os.log
 import SwiftUI
 
 /// Central design token system for Wirc.
@@ -29,7 +30,7 @@ enum DesignSystem {
         static let pencil = Color(uiColor: UIColor { trait in
             trait.userInterfaceStyle == .dark
                 ? UIColor(hex: "B8B0A8")
-                : UIColor(hex: "78716C")
+                : UIColor(hex: "615E5A")
         })
         /// Hairline rules, dividers
         static let border = Color(uiColor: UIColor { trait in
@@ -141,21 +142,15 @@ enum DesignSystem {
 extension Color {
     init(hex: String) {
         let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
-        var int: UInt64 = 0
-        Scanner(string: hex).scanHexInt64(&int)
-        let r, g, b: UInt64
-        switch hex.count {
-        case 6:
-            (r, g, b) = ((int >> 16) & 0xFF, (int >> 8) & 0xFF, int & 0xFF)
-        default:
-            (r, g, b) = (0, 0, 0)
+        guard hex.count == 6, let int = UInt64(hex, radix: 16) else {
+            os_log(.error, "DesignSystem: invalid hex color '%{public}@', falling back to black", hex)
+            self = .black
+            return
         }
-        self.init(
-            .sRGB,
-            red: Double(r) / 255,
-            green: Double(g) / 255,
-            blue: Double(b) / 255
-        )
+        let r = Double((int >> 16) & 0xFF) / 255.0
+        let g = Double((int >> 8) & 0xFF) / 255.0
+        let b = Double(int & 0xFF) / 255.0
+        self.init(red: r, green: g, blue: b)
     }
 }
 
